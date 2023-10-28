fadffde568a8355cc705b37055b603758bdafaec
Kosta Dinkov <k.v.dinkov@gmail.com>
2023-10-28 10:41:44 +0300

    fix nav menu

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -12,21 +12,21 @@ export default function Nav({ position }) {
         <li>
           <a href="#">Home</a>
         </li>
-        |
+        
         <li>
           <a href="#">About</a>
         </li>
-        |
+        
         <li>
           <a href="#">Menu</a>
         </li>
-        |
+        
         <li>
           <a href="#">Reservations</a>
-        </li>|
+        </li>
         <li>
           <a href="#">Order Online</a>
-        </li>|
+        </li>
         <li>
           <a href="#">Login</a>
         </li>
