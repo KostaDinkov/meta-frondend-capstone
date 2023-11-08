e38aa030fa0d91853cac102129925188d925b0bf
Kosta Dinkov <k.v.dinkov@gmail.com>
2023-11-08 09:53:22 +0200

    fix: handle  no available times for given date and guests

--- src/Components/BookingForm/SubmitForm.js
+++ src/Components/BookingForm/SubmitForm.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Selector from "./Selector";
 import styles from "./styles.module.scss";
 import Spinner from "../Elements/Spinner";
 import { resetField } from "./utils";
-import {format} from 'date-fns'
+import { format } from "date-fns";
 
 export default function SubmitForm({
   formState,
@@ -11,52 +11,77 @@ export default function SubmitForm({
   handleFieldChange,
   handleSubmit,
 }) {
+  const [isAvailable, setIsAvailable] = useState(false);
+  const availableText = `We found available tables for ${formState.guests.value} ${
+    formState.guests.value > 1 ? "guests" : "guest"
+  } for ${format(
+    new Date(formState.date.value),
+    "PPPP"
+  )}. Select time from the list below.`
+  const availableHeading = "Choose Time"
+  const unavailableHeading = "Sorry!"
+  const unavailableText = `We couldn't find available tables for ${formState.guests.value} ${
+    formState.guests.value > 1 ? "guests" : "guest"
+  } for ${format(
+    new Date(formState.date.value),
+    "PPPP"
+  )}`
+  
+  useEffect(() => {
+    setIsAvailable(formState.availableTimes.length > 0);
+  }, [formState]);
+
   return (
     <>
-      <h1>Choose Time</h1>
-      <p>{`We found available tables for ${formState.guests.value} ${
-            formState.guests.value > 1 ? "guests" : "guest"
-          } for ${
-            format(new Date(formState.date.value), "PPPP")
-          }. Select time from the list below.`}</p>
+      <h1>{isAvailable?availableHeading:unavailableHeading}</h1>
+      <p>{isAvailable?availableText:unavailableText}</p>
 
       <form className={styles.bookingForm} onSubmit={handleSubmit}>
-       
-        <Selector
-          name="time"
-          label="Time"
-          value={formState.time.value}
-          options={formState.availableTimes}
-          defaultOption={"Select time"}
-          error={{
-            status: !formState.time.isValid,
-            message: formState.time.errorMessages[0],
-          }}
-          handleFieldChange={handleFieldChange}
-        />
-        <Selector
-          name="occasion"
-          label="Occasion"
-          value={formState.occasion.value}
-          handleFieldChange={handleFieldChange}
-          options={["No occasion", "Birthday", "Anniversary"]}
-        />
-        <div className={styles.submitBtnGroup}>
-            <button
-            className={styles.button}
-              disabled={formState.isLoading}
-              onClick={(e) => {
-                e.preventDefault()
-                setFormState({ ...formState, stage: 0, time:resetField(), occasion:resetField() });
+        {isAvailable && (
+          <>
+            <Selector
+              name="time"
+              label="Time"
+              value={formState.time.value}
+              options={formState.availableTimes}
+              defaultOption={"Select time"}
+              error={{
+                status: !formState.time.isValid,
+                message: formState.time.errorMessages[0],
               }}
-            >
-              Back
-            </button>
-            <button
+              handleFieldChange={handleFieldChange}
+            />
+            <Selector
+              name="occasion"
+              label="Occasion"
+              value={formState.occasion.value}
+              handleFieldChange={handleFieldChange}
+              options={["No occasion", "Birthday", "Anniversary"]}
+            />
+          </>
+        )}
+        <div className={isAvailable?styles.submitBtnGroup:styles.submitBtnGroupCenter}>
+          <button
+            className={styles.button}
+            disabled={formState.isLoading}
+            onClick={(e) => {
+              e.preventDefault();
+              setFormState({
+                ...formState,
+                stage: 0,
+                time: resetField(),
+                occasion: resetField(),
+              });
+            }}
+          >
+            Back
+          </button>
+          {isAvailable && <button
             className={styles.buttonMain}
-              disabled={!formState.isValid || formState.isLoading}
-
-            >Book now</button>
+            disabled={!formState.isValid || formState.isLoading}
+          >
+            Book now
+          </button>}
         </div>
         <Spinner visible={formState.isLoading} />
       </form>
